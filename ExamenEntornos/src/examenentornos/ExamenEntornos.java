36b30b78aa3d2fcbc38ae1d881d085b6e92cc291
eltal <eltal@DESKTOP-SUH96B2>
2016-03-11 11:34:13 +0100

    #1 - Proyecto Original

---
+++ ExamenEntornos/src/examenentornos/ExamenEntornos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenentornos;
+
+/**
+ *
+ * @author eltal
+ */
+public class ExamenEntornos {
+
+    public void multi(int a, int b){
+        System.out.println(3*4);
+    }
+    public static void main(String[] args) {
+    ExamenEntornos prueba = new ExamenEntornos();
+    prueba.multi(3,4);
+    }
+    
+}
